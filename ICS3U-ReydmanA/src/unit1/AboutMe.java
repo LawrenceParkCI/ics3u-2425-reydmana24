0eb6dba13d960517dc197182ff95a9ac3e64bb47
342881794 <342881794@MXL1502T8S.tdsb.on.ca>
2024-09-20 11:38:27 -0400

    This is about me. Telling my name, grade, teacher and school.

---
+++ ICS3U-ReydmanA/src/unit1/AboutMe.java
@@ -0,0 +1,19 @@
+package unit1;
+/**
+ * Description: This program prints my name, grade, teacher, and school. <br>
+ * Date: Sept. 20, 2024
+ * @author Alex Reydman 
+ */
+public class AboutMe {
+
+	public static void main(String[] args) {
+		// This prints to the system console
+		System.out.println(" About Me ");
+		System.out.println("________________________");
+		System.out.println(" My name is AlexReydman ");
+		System.out.println(" I am in Grade 11 ");
+		System.out.println(" My teacher is Ms. Kemp ");
+		System.out.println(" I go to Lawrence Park Collegiate Institute ");
+	}
+
+}
