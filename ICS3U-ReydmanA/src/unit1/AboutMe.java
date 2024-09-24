9f70d2009978e2f3f648bd4b3ef97c2f8aa306e6
342881794 <342881794@MXL1502T8S.tdsb.on.ca>
2024-09-24 11:28:03 -0400

    half way there for time table.

--- ICS3U-ReydmanA/src/unit1/AboutMe.java
+++ ICS3U-ReydmanA/src/unit1/AboutMe.java
@@ -8,13 +8,34 @@ public class AboutMe {
 
 	public static void main(String[] args) {
 		// This prints to the system console
-		System.out.println(" About Me ");
+		System.out.println("About Me ");
 		System.out.println("________________________");
-		System.out.println(" My name is AlexReydman ");
-		System.out.println(" I am in Grade 11 ");
-		System.out.println(" My teacher is Ms. Kemp ");
-		System.out.println(" I go to Lawrence Park Collegiate Institute ");
-		System.out.println(" Go Panthers");
+		System.out.println("My name is AlexReydman ");
+		System.out.println("I am in Grade 11 ");
+		System.out.println("My teacher is Ms. Kemp ");
+		System.out.println("I go to Lawrence Park Collegiate Institute ");
+		System.out.println("\"Go Panthers!\"");
+		System.out.println("-------------------------------------------------------------------------------------------------------------------------------------");
+		System.out.format("	Date:");
+		System.out.format(" 		|\tPeriod 1 \t| \t Period 2 \t| \t Period 3 \t| \t Period 4 \t|");
+		System.out.println ("\n");
+		System.out.format(" 			|     9:00am-10:15am \t|    10:20am - 11:40am  |    12:40pm - 1:55pm   |    2:00pm - 3:15pm    |");
+		System.out.println ("\n");
+		System.out.println ("\n");
+		System.out.format("	Monday:");
+		System.out.println ("\n");
+		System.out.println ("\n");
+		System.out.format("	Tuesday:");
+		System.out.println ("\n");
+		System.out.println ("\n");
+		System.out.format("	Wedensday:");
+		System.out.println ("\n");
+		System.out.println ("\n");
+		System.out.format("	Thursday:");
+		System.out.println ("\n");
+		System.out.println ("\n");
+		System.out.format("	Friday:");
+
 	}
 
 }
