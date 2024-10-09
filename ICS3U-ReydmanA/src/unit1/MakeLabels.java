4e82bc62a56071cefeb2c9683aa3f6d7ed8cc7b2
andre <andre@DESKTOP-A0JTQ6C.home>
2024-10-08 22:20:20 -0400

    String Variables Worksheet

--- ICS3U-ReydmanA/src/unit1/MakeLabels.java
+++ ICS3U-ReydmanA/src/unit1/MakeLabels.java
@@ -3,7 +3,7 @@ package unit1;
 import java.util.Scanner;
 
 public class MakeLabels {
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 		Scanner sc = new Scanner (System.in);
 		
 		String subject;
