2343f2700f3a95eb68c005233e8af4e43043c5af
342881794 <342881794@MXL1502T8S.tdsb.on.ca>
2024-10-10 11:37:48 -0400

    Commiting Math Methods and casting 1

--- ICS3U-ReydmanA/src/unit1/UsingMathMethods.java
+++ ICS3U-ReydmanA/src/unit1/UsingMathMethods.java
@@ -1,12 +1,11 @@
 package unit1;
-
+/*
+ * Description: Using code to to math problems
+ * Date: 2024-10-10]
+ * @Alex Reydman
+ */
 import java.util.Scanner;
 
-/**
- * Description:
- * Date:
- * @author 
- */
 public class UsingMathMethods {
 
 	public static void main(String[] args) {
@@ -15,76 +14,80 @@ public class UsingMathMethods {
 
 		System.out.println("Part A");
 
-		//Write code to ask the user for any real number to be input.
-		//Store their input into the num variable
+		// Asking the user for a real number
+		System.out.print("Please enter a real number: ");
+		num = sc.nextDouble();
 
-		System.out.println(Math.round(num));
-		System.out.println(Math.rint(num));
-		System.out.println(Math.sqrt(num));
-		System.out.println(Math.abs(num));
-		System.out.println(Math.pow(num, 2));
+		System.out.println("Math.round(num) = " + Math.round(num));
+		System.out.println("Math.rint(num) = " + Math.rint(num));
+		System.out.println("Math.sqrt(num) = " + Math.sqrt(num));
+		System.out.println("Math.abs(num) = " + Math.abs(num));
+		System.out.println("Math.pow(num, 2) = " + Math.pow(num, 2));
+		System.out.println("Math.pow(num, num) = " + Math.pow(num, num));
+		System.out.println("Math.pow(num, 3) = " + Math.pow(num, 3));
 
 		/*
-		 * Run the program and write down the output that is produced
-		 * when each of the following values are input:
+		 * After running the program, fill in the output for the following inputs:
 		 * 
-		 * 					num = 16.75		| num = 4.0		| num = -23.45
-		 * 					-----------------------------------------------
-		 * Math.round(num)					| 				|
-		 * Math.rint(num)					| 				|
-		 * Math.sqrt(num)					|				|
-		 * Math.abs(num)					|				|
-		 * Math.pow(num, 2)					|				|
-		 * Math.pow(num, num)				|				|
-		 * Math.pow(num, 3)					|				|
-		 * 
+		 * num = 16.75		| num = 4.0		| num = -23.45
+		 * --------------------------------------------------------
+		 * Math.round(num)   | 17             | 4              | -23
+		 * Math.rint(num)    | 17.0           | 4.0            | -23.0
+		 * Math.sqrt(num)    | 4.093          | 2              | NaN
+		 * Math.abs(num)     | 16.75          | 4              | 23.45
+		 * Math.pow(num, 2)  | 280.5625       | 16             | 552.9025
+		 * Math.pow(num, num)| 500217.05119...| 256           | NaN
+		 * Math.pow(num, 3)  |  2959.0625     | 64             | -1220.709125
 		 */
 
-		//Why does the last value(-23.45) give strange output for Math.sqrt(num)?
-		//Nan stands for Not a number!
+		// Why does the last value (-23.45) give strange output for Math.sqrt(num)?
+		// Nan stands for Not a number! This is because the square root of a negative number is not defined in the set of real numbers.
 
-		//In your own words describe what the following Math methods does
+		// In your own words describe what the following Math methods does
 		/*
-		 * round:
-		 * rint:
-		 * sqrt:
-		 * abs:
-		 * pow:
+		 * round: Rounds a floating-point number to the nearest integer.
+		 * rint: Returns the double value that is closest to the argument and is equal to a mathematical integer.
+		 * sqrt: Returns the square root of a number.
+		 * abs: Returns the absolute value of a number.
+		 * pow: Raises a number to the power of another number.
 		 */
 
-		//The methods round, sqrt and abs only have one parameter (which is in the brackets).
-		//Why does the pow method have 2 parameters? Explain the purpose of these 2 parameters.
-
+		// The methods round, sqrt, and abs only have one parameter. Why does the pow method have 2 parameters?
+		// The pow method requires two parameters: the base (the number to be raised) and the exponent (the power to raise it to).
 
 		System.out.println("Part B");
-		//Ask the user for any real number and an integer to be input. Display the real number,
-		//and its square root. Also determine and display the real number raised to the integer power.
-		//Use suitable headings, displaying the results across the screen.\
-		//
-		//Eg. If the real number 13.5 nad the integer 2 are input, display
-		//REAL NUMBER	SQUARE ROOT		RAISED TO POWER 2 <- display power here
-		//   13.5		   3.6742			182.25
+		// Ask the user for a real number and an integer
+		System.out.print("Please enter a real number: ");
+		double realNum = sc.nextDouble();
+		System.out.print("Please enter an integer: ");
+		int power = sc.nextInt();
 
+		double sqrt = Math.sqrt(realNum);
+		double raisedToPower = Math.pow(realNum, power);
 
+		System.out.printf("REAL NUMBER\tSQUARE ROOT\tRAISED TO POWER %d%n", power);
+		System.out.printf("%10.4f\t%10.4f\t%10.4f%n", realNum, sqrt, raisedToPower);
 
+		// Results for real number 7.8985 and integer 3
+		// Output should be:
+		// REAL NUMBER      SQUARE ROOT       RAISED TO POWER 3
+		//      7.8985         2.8101              615.0364
 
-		//Write down the results produced by your program when the real number 7.8985 and
-		//the integer 3 are input.
+		// Change to accept a real number power
+		System.out.print("Please enter a real number for power: ");
+		double realPower = sc.nextDouble();
+		double raisedToRealPower = Math.pow(realNum, realPower);
+		System.out.printf("REAL NUMBER\tSQUARE ROOT\tRAISED TO POWER %.2f%n", realPower);
+		System.out.printf("%10.4f\t%10.4f\t%10.4f%n", realNum, sqrt, raisedToRealPower);
 
+		// a) Input: 16 and 0.5
+		// Expected output: 16.0  4.0  4.0
+		// b) Input: 8 and 0.33
+		// Expected output: 8.0  2.8284  2.0 (approximately)
 
+		// Does the program give a different answer if 0.3333333 is input instead of 0.33 for the power?
+		// The output will be slightly different due to the precision of floating-point representation.
 
-		//Change your program so that instead of inputting an integer power, a real number
-		//power can be input.
-		//Write down the results produced by your program when the following are input:
-		//a) the number 16 and the real number power 0.5
-		//b) the number 8 and the real number power 0.33
-
-
-		//Does your program give a different answer if 0.3333333 is input instead of 0.33 for the power?
-
-
-
-
+		sc.close();
 	}
-
 }
