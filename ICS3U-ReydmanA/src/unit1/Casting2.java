8feee7a4e316d78496f6a49ac2523fe13c1c6c35
342881794 <342881794@MXL1502T8S.tdsb.on.ca>
2024-10-17 09:22:05 -0400

    Finished Casting 2

--- ICS3U-ReydmanA/src/unit1/Casting2.java
+++ ICS3U-ReydmanA/src/unit1/Casting2.java
@@ -1,38 +1,45 @@
 package unit1;
 
 /**
-* Description:
-* Date: 2024-10-10
-* @Alex Reydman
-*/
+ * Description: This demonstrates type casting between char and int, showing implicit and explicit casting.
+ * Date: 2024-10-10
+ * @Alex Reydman
+ */
 public class Casting2 {
-  
-  /**
-   * This is the entry point to the program.
-   * @param args unused
-   */
-  public static void main(String[] args) {
-    /*
-    Char and int
-    */
-    char myChar = 'a';
-    System.out.println(myChar);
-    System.out.println(myChar + 1);
 
-    char myChar2 = (char) (myChar + 1);
+	/**
+	 * This is the entry point to the program.
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		/*
+		    Char and int
+		 */
+		char myChar = 'a';
+		System.out.println(myChar);
+		System.out.println(myChar + 1);
 
-    System.out.println(myChar2);
+		char myChar2 = (char) (myChar + 1);
 
-    //Explain what is happening in the code. Where is there explicit casting, where is there implicit casting?
-    //there is explicit casting in the first line system.out.println(myChar);
-    //There is explicit cast
-    //Why do you think this happens?
-    //Investigate what is ASCII. What is it?
-    //Can you find the number value for 'a'? Does it match with your findings above?
+		System.out.println(myChar2);
 
-    //create a new character myCharCap, and transform myChar into a capital 'A' and print it
+		//Explain what is happening in the code. Where is there explicit casting, where is there implicit casting?
+		//The code starts by defining "mychar" as "a". When the number 1 is added to mychar, java reads it as a number, resulting in 98. Then to convert it back to letters the code uses a new variable mychar2 as a letter "b".  
 
-    
+		//Why do you think this happens?
+		//Because Java uses numeric values (ASCII) to represent characters. When you add to a character, it converts to a number. You then will need explicit casting to turn the number back into a character after.
 
-  }
+		//Investigate what is ASCII. What is it?
+		//ASCII (American Standard Code for Information Interchange) is a character encoding standard that assigns numeric values (0-127) to characters, including letters, digits, and symbols. 
+		//Can you find the number value for 'a'? Does it match with your findings above?
+		//Yes the value for 'a' in ASCII is '97'. It does match my findings above
+
+		//create a new character myCharCap, and transform myChar into a capital 'A' and print it
+
+		char myChar1 = 'a';
+
+		System.out.println((char)(myChar1 - 32));
+
+
+	}
 }
