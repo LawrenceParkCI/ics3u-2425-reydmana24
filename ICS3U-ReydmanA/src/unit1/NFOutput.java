c4063ad6da56b68e222839be586c7712404584b3
342881794 <342881794@MXL1502T8S.tdsb.on.ca>
2024-11-11 09:21:01 -0500

    Pushing multiple missing assignments.

--- ICS3U-ReydmanA/src/unit1/NFOutput.java
+++ ICS3U-ReydmanA/src/unit1/NFOutput.java
@@ -1,5 +1,43 @@
 package unit1;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+/**
+ * Description: NF Output class
+ * Date: Oct 27, 2024
+ * @author Alex Reydman
+ */
 public class NFOutput {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		final double TAX_RATE = 0.13;
+		NumberFormat money = NumberFormat.getCurrencyInstance();
+		NumberFormat percent = NumberFormat.getPercentInstance();
+
+		double cost;
+		System.out.print("Please enter the cost of an item then press <Enter>: $");
+		cost = sc.nextDouble();
+		double total = cost + (cost * TAX_RATE);
+		System.out.println("The cost of your item with tax is " + money.format(total));
+		System.out.println("The tax rate is " + percent.format(TAX_RATE));
+		
+		//How can you change how your value is displayed?
+		
+		// A different NumberFormat code can be used
+		
+		//What other formats does NumberFormat offer?
+		
+		/* Other formats does NumberFormat offer are:
+		 * NumberFormat.getDecimalInstance();
+		 * NumberFormat.getIntegerInstance
+		 * NumberFormat.getUnitInstance();
+		 * NumberFormat.getCompactNumberInstance();
+		 
+		 */
+		
+
+		sc.close();
+	}
 
 }
